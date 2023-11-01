2edb9fa0916ea031c82d2280dda4d80eee39e6e9
joseph <hong.joseph97@gmail.com>
2023-11-01 19:19:45 -0400

    added pca test example

--- dim_reduce.py
+++ dim_reduce.py
@@ -1,7 +1,24 @@
 '''
-Vector Reduction
+Vector Dimension Class for testing different methods.
 '''
 
-class VectorReduction:
-    def __init__(self, type):
-        self.type = type
+import numpy as np
+from sklearn.decomposition import PCA
+
+class DimensionReduction:
+    def __init__(self, name, dim):
+        self.name = name
+        self.dim = dim
+        self.type = None
+
+        if self.name == 'PCA':
+            self.type = PCA(n_components=dim)
+    
+    def fit(self, data):
+        """
+        data format (n_samples, n_features)
+        """
+        self.type.fit(data)
+    
+    def transform(self, data):
+        self.type.transform(data)
